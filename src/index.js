a9e912e17ce5f2188cbd38f65cbb51817f00547f
mmilett14 <mmilett14@gmail.com>
2021-05-18 20:49:03 -0500

    added aws authentication

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
 
 ReactDOM.render(
     <App />,
